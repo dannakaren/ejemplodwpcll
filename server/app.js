3c56cf4bde972c7b4aff9dea20159ef267dcffea
dannakaren <134405560+dannakaren@users.noreply.github.com>
2023-09-18 07:55:37 +0000

    💫Enhace: webpack

--- server/app.js
+++ server/app.js
@@ -5,29 +5,32 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var debug = require('debug')('ejemplodwpcll:server');
 
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-//Creando la innstancia de express
+// Creando la instancia de express
 var app = express();
 
-//Configurando el motor de plantillas
-//debug(`📣Ruta actual: ${path.join(__dirname, 'views')}`);
+// Configurando el motor de plantillas
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-//se establecen los milddlewares
+// Se establecen los middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-//Crear un server de archivos estaticos
+// Crea un server de archivos estaticos
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
+// Registro de Middlewares de aplicación
 app.use('/', indexRouter);
-//app.use('/users', usersRouter);
+// Activa "usersRourter" cuando se
+// solicita "/users"
+app.use('/users', usersRouter);
+// app.use('/author', (req, res)=>{
+//   res.json({mainDeveloper: "Danna Gutierrez"})
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
