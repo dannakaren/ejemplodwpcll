b15e55c9aabcb9b92a91367c0f4de1d5e8a9b91e
dannakaren <134405560+dannakaren@users.noreply.github.com>
2023-09-18 05:57:07 +0000

    Instalacion

---
+++ webpack.dev.config.js
@@ -0,0 +1,57 @@
+// Notas importantes
+// El archivo de configuración debe usar ES5
+
+// Importar un administrador de rutas de archivos
+const path = require('path');
+
+// Exportamos un objeto de configuración
+// que será usado por webpack
+module.exports = {
+  // 1. El archivo de entrada o indexador
+  entry: "./client/index.js",
+  // 2. Especificar el archivo de salida
+  output: {
+    // 2.1 Ruta absoluta de salida
+    path: path.resolve(__dirname, "public"),
+    // 2.2 Nombre del archivo de salida
+    filename: "bundle.js",
+    // 2.3 Ruta base de archivos estáticos
+    publicPath: "/"
+  },
+  // 3. Configurando el servidor de desarrollo
+  devServer: {
+    // 3.1 Folder de archivos estáticos
+    static: path.join(__dirname, "public"),
+    // 3.2 Puerto del servidor de desarrollo
+    port: 8080,
+    // 3.3 Definiendo el host
+    host: "0.0.0.0"
+  },
+   // Agregando un modulo a webpack
+   module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    'modules': false,
+                    'useBuiltIns': 'usage',
+                    'targets': '> 0.25%, not dead',
+                    'corejs': 3
+                  }
+                ]
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
