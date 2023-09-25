16c7d9a3db046f0252729fce3af1a564919f6c66
dannakaren <134405560+dannakaren@users.noreply.github.com>
2023-09-25 04:05:13 +0000

    examen

--- routes/about.js
+++ routes/about.js
@@ -4,7 +4,8 @@ const router = express.Router();
 
 const imagenes = ['https://th.bing.com/th/id/R.2708e757cf8d1ad31bfa4412cf500633?rik=bD9Hd5oSlb2tCw&riu=http%3a%2f%2f1.bp.blogspot.com%2f_wsl8GstV6SU%2fTG4EKl96uTI%2fAAAAAAAAAAM%2f2_PGSZz0yDQ%2fs1600%2fLogoVectorizado%2bITGAM_.jpg&ehk=JtVkEqbASeDg%2bbvPkPDOhAaWPionkj2cidv7FN4Mil4%3d&risl=&pid=ImgRaw&r=0',
   'https://th.bing.com/th/id/OIP.uTKyGFj7Z2ICaKCL8-fxIAHaC6?pid=ImgDet&rs=1',
-  'https://yt3.ggpht.com/a/AATXAJywPUTxfk_0y0qGRMQFCwqb1PUU3fwHiUMvMQ=s900-c-k-c0xffffffff-no-rj-mo'
+  'https://yt3.ggpht.com/a/AATXAJywPUTxfk_0y0qGRMQFCwqb1PUU3fwHiUMvMQ=s900-c-k-c0xffffffff-no-rj-mo',
+   'https://th.bing.com/th/id/R.9a34696a2715815045c1dc8962b845d5?rik=r5%2bDhzlLGcgIkw&riu=http%3a%2f%2fgamadero.tecnm.mx%2fimagenes%2fnoticias%2foctubre2019%2fnoticia+convocatoria+nuevo+ingreso.jpg&ehk=pOHetwxTszZhUYb9ybBG77Tqcpxkf%2fhrXFfOcTSbxnE%3d&risl=&pid=ImgRaw&r=0'
 ];
 
 function seleccionarImagenAlAzar(imagenes) {
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  router.get('/', function (_, res) {
   res.render('index', { title: 'ITGAM', autho: "Danna Gutierrez" });
 });
+
+/* GET/user/about. */
+router.get('/tec', function(req, res, next) {
+  res.render('about', { nombre: "Danna Gutierrez" });
 });
+
+
 module.exports = router;
