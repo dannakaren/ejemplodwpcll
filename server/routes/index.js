ba87458f4c7205aaae8336bf76665f551d1552f4
dannakaren <134405560+dannakaren@users.noreply.github.com>
2023-09-27 17:33:32 +0000

    avance 2

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ITGAM', author:"Danna Gutierrez" });
+/* GET Home page */
+router.get('/', function(){
+  res.render('index', { title: 'Express with babel' });
 });
 
 module.exports = router;
