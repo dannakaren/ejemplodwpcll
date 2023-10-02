6842941b30f5668dc890ab30d3e52c07ea34fefe
dannakaren <134405560+dannakaren@users.noreply.github.com>
2023-10-02 06:49:41 +0000

    🎇modo servicio

--- server/services/debugLogger.js
+++ server/services/debugLogger.js
@@ -1,6 +1,6 @@
 // Importando dependencia debug
 import Debug from 'debug'
 // Establecemos el proceso a debuggear
-const debug = Debug('dwpcii:server')
+const debug = Debug('ejemplodwpcll:server')
 // exportando el logger de debuggeo
 export default debug
