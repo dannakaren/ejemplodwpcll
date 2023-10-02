668c87f6e6f17283f35827623c2f7985e4dac8cf
dannakaren <134405560+dannakaren@users.noreply.github.com>
2023-10-02 06:19:46 +0000

    modo desarrollo

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var debug = require('debug')('ejemplodwpcll:server');
-import debug from './services/debugLogger'
+//import debug from './services/debugLogger'
 
 
 var indexRouter = require('./routes/index');
