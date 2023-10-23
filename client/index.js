5411bf9848f03ae1600bc1e949f00e034a799dc1
dannakaren <134405560+dannakaren@users.noreply.github.com>
2023-10-23 00:24:23 +0000

    💈Mejorando la Navbar

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-console */
 import './styles/style.css';
 /* eslint-disable */
-// Importando estilos de Materialize CSS
+// Importandoe Estilos de Materialize CSS
 import 'materialize-css/dist/css/materialize.css';
 // Importando scripts de Materialize
 import 'materialize-css/dist/js/materialize';
