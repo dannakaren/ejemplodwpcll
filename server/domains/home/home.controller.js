e84cf0d0d8962001ff0bab142e1947aeb58ab875
dannakaren <134405560+dannakaren@users.noreply.github.com>
2023-10-27 04:18:55 +0000

    Adding Views about and home

--- server/domains/home/home.controller.js
+++ server/domains/home/home.controller.js
@@ -2,11 +2,17 @@
 // GET "/"
 // GET "/index"
 const home = (req, res) => {
-  const iconSet = ['⭐', '🤖', '🍉'];
-  const icon = iconSet[Math.floor(Math.random() * 3)];
-  res.render('index', { title: 'ejemplodwpcll', icon });
+  const iconSet = ['⭐', '🤖', '🍉', '🕷', '🌈'];
+  const icon = iconSet[Math.floor(Math.random() * iconSet.length)];
+  res.render('home/homeView', { icon });
 };
+// GET "/about"
+const about = (req, res) => {
+  res.render('home/aboutView', { appVersion: '1.0.0' });
+};
+
 // Controlador Home
 export default {
   home,
+  about,
 };
