01ad89b36c0487025e29660bfa82cd6fa88d71c0
dannakaren <134405560+dannakaren@users.noreply.github.com>
2023-11-20 07:59:14 +0000

    errores2

--- server/app.js
+++ server/app.js
@@ -1,34 +1,35 @@
-// Cargando dependencias
+// Helps to handle http errors
+import createError from 'http-errors';
+// Import the Express Library
 import express from 'express';
+// Is a Core-Node library to manage system paths
 import path from 'path';
+// Helps to parse client cookies
 import cookieParser from 'cookie-parser';
+// Library to log http communication
 import morgan from 'morgan';
-import mongoose from 'mongoose';
+
+// Importing template-engine
+import configTemplateEngine from '@server/config/templateEngine';
 
 // Setting Webpack Modules
 import webpack from 'webpack';
 import WebpackDevMiddleware from 'webpack-dev-middleware';
 import WebpackHotMiddleware from 'webpack-hot-middleware';
-
-// Importing template-engine
-import configTemplateEngine from './config/templateEngine';
-
 // Importing webpack configuration
 import webpackConfig from '../webpack.dev.config';
 
 // Impornting winston logger
 import log from './config/winston';
 
-// Importing Router
+// Importando enrutador
 import router from './router';
-
-import debug from './services/debugLogger';
 
 // Creando variable del directorio raiz
 // eslint-disable-next-line
 global['__rootdir'] = path.resolve(process.cwd());
 
-// Creando la instancia de express
+// We are creating the express instance
 const app = express();
 
 // Get the execution mode
@@ -37,10 +38,10 @@ const nodeEnviroment = process.env.NODE_ENV || 'production';
 // Deciding if we add webpack middleware or not
 if (nodeEnviroment === 'development') {
   // Start Webpack dev server
-  debug('🛠️ Ejecutando en modo desarrollo 🛠️');
+  console.log('🛠️  Ejecutando en modo desarrollo');
   // Adding the key "mode" with its value "development"
   webpackConfig.mode = nodeEnviroment;
-  // Setting the dev server port to the same value as the express server
+  // Setting the port
   webpackConfig.devServer.port = process.env.PORT;
   // Setting up the HMR (Hot Module Replacement)
   webpackConfig.entry = [
@@ -66,26 +67,38 @@ if (nodeEnviroment === 'development') {
 
 // Configuring the template engine
 configTemplateEngine(app);
-//  Database connecition Checker Middleware
+
+// Registering middlewares
+// Log all received requests
+app.use(morgan('dev', { stream: log.stream }));
+// Parse request data into json
+app.use(express.json());
+// Decode url info
+app.use(express.urlencoded({ extended: false }));
+// Parse client cookies into json
+app.use(cookieParser());
+// Set up the static file server
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Registering routes
+router.addRoutes(app);
+
+// catch 404 and forward to error handler
 app.use((req, res, next) => {
-  if (mongoose.connection.readyState === 1) {
-    log.info('✅ Verificacion de conexion a db exitosa');
-    next();
-  } else {
-    log.info('🔴 No pasa la verificacion de conexion a la db');
-    res.status(503).render('errors/e503View', { layout: 'errors' });
-  }
+  log.info(`404 Pagina no encontrada ${req.method} ${req.originalUrl}`);
+  next(createError(404));
 });
 
-// Se establecen los middlewares
-app.use(morgan('dev', { stream: log.stream }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-// Crea un server de archivos estaticos
-app.use(express.static(path.join(__dirname, '..', 'public')));
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// Registro de Rutas
-router.addRoutes(app);
+  // render the error page
+  res.status(err.status || 500);
+  log.error(`${err.status || 500} - ${err.message}`);
+  res.render('error');
+});
 
 export default app;
--- server/domains/project/project.controller.js
+++ server/domains/project/project.controller.js
@@ -4,30 +4,33 @@ import log from '../../config/winston';
 // Actions methods
 // GET "/project"
 const showDashboard = (req, res) => {
+  res.send('⚠️ UNDER CONSTRUCTION: GET /project ⚠️');
+};
+
+// GET "/project/add"
+const add = (req, res) => {
+  res.render('project/addView');
+};
+
+// POST "/project/add"
 const addPost = (req, res) => {
+  // Rescatando la info del formulario
+  const { errorData: validationError } = req;
+  // En caso de haber error
   // se le informa al cliente
   if (validationError) {
     log.info('Se entrega al cliente error de validación de add Project');
-    // Se desestructuran los datos de validación
-    const { value: project } = validationError;
-    // Se extraen los campos que fallaron en la validación
-    const errorModel = validationError.inner.reduce((prev, curr) => {
-      // Creando una variable temporal para
-      // evitar el error "no-param-reassing"
-      const workingPrev = prev;
-      workingPrev[`${curr.path}`] = curr.message;
-      return workingPrev;
-    }, {});
-    return res.status(422).render('project/addView', { project, errorModel });
+    res.status(422).json(validationError);
+  } else {
+    // En caso de que pase la validación
+    // Se desestructura la información
+    // de la peticion
+    const { validData: project } = req;
+    // Se contesta la información
+    // del proyecto al cliente
+    log.info('Se entrega al cliente información del proyecto cargado');
+    res.status(200).json(project);
   }
-  // En caso de que pase la validación
-  // Se desestructura la información
-  // de la peticion
-  const { validData: project } = req;
-  // Se contesta la información
-  // del proyecto al cliente
-  log.info('Se entrega al cliente información del proyecto cargado');
-  res.status(200).json(project);
 };
 
 // Controlador user
