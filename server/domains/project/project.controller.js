e4c22dc9e782a93ef0786c80018b37f1063348b2
dannakaren <134405560+dannakaren@users.noreply.github.com>
2023-11-24 06:49:04 +0000

    correcion de errores

--- server/domains/project/project.controller.js
+++ server/domains/project/project.controller.js
@@ -1,15 +1,16 @@
-// Importing winston logger
 import log from '../../config/winston';
 
 // Importando el modelo
 import ProjectModel from './project.model';
 
-// Importando Httperrors
-
 // Actions methods
 // GET "/project"
 const showDashboard = (req, res) => {
+  res.send('⚠️ UNDER CONSTRUCTION: GET /project ⚠️');
+};
+// GET "/project/add"
 const add = (req, res) => {
+  res.render('project/addView');
 };
 
 // POST "/project/add"
@@ -17,13 +18,25 @@ const addPost = async (req, res) => {
   // Rescatando la info del formulario
   const { errorData: validationError } = req;
   // En caso de haber error
-const addPost = (req, res) => {
+  // se le informa al cliente
+  if (validationError) {
+    log.info('Se entrega al cliente error de validación de add Project');
+    // Se desestructuran los datos de validación
+    const { value: project } = validationError;
+    // Se extraen los campos que fallaron en la validación
+    const errorModel = validationError.inner.reduce((prev, curr) => {
+      // Creando una variable temporal para
+      // evitar el error "no-param-reassing"
+      const workingPrev = prev;
+      workingPrev[`${curr.path}`] = curr.message;
+      return workingPrev;
+    }, {});
+    return res.status(422).render('project/addView', { project, errorModel });
+  }
+  // En caso de que pase la validación
   // Se desestructura la información
   // de la peticion
   const { validData: project } = req;
-  // Creando la instancia de un documento
-  // con los valores de 'project'
-  const projectDocument = new ProjectModel(project);
   try {
     // Creando la instancia de un documento con los valores de 'project'
     const savedProject = await ProjectModel.create(project);
